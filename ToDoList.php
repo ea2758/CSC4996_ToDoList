acfe3d0dba57a03f5afeb958a82510ea52916a92
Daniel Warrick <ea2758@wayne.edu>
2017-01-17 14:56:12 -0800

    Add New items button now working. Contents of the text field adjacent to the button can be added to the table, Uncompleted_Tasks. The task number variable is still not incrementing correctly though.

--- ToDoList.php
+++ ToDoList.php
@@ -6,12 +6,12 @@
 	<center><table border = "3" width = "400" height = "300">
 	
 	<tr>
-	<td align="center"><input type = "submit" name="View List" value="View List"></td>
+	<td align="center"><input type = "submit" name="viewList" value="View List"></td>
 	</tr>
 	<br>
 	
 	<tr>
-	<td align="center"><input type = "submit" name="Add Item" value="Add Item">
+	<td align="center"><input type = "submit" name="addItem" value="Add Item">
 						<input type = "name" name="newItem"></td>
 	</tr>
 	<br>
@@ -23,21 +23,20 @@
 </table></center>
 </body>
 </html>
-
+<?php $counter=1;?>
 <?php
-$counter = 1;
 $link = mysqli_connect("localhost","root","");
 if($link==false){
 	die("Error: connection error");
 }
 mysqli_select_db($link,"ToDoList") or die("error: cannot locate database");
 
-if(isset($_POST['submit'])){
-	echo "submit read";
-	$item = mysqli_real_escape_string($link, $_POST['newItem']);
+if(isset($_POST["addItem"])&&!empty($_POST["addItem"])){
+	$item = mysqli_real_escape_string($link, $_POST["newItem"]);
 	$insertSQL = "INSERT INTO uncompleted_tasks(task_number,uncompleted_task)VALUES('$counter','$item')";
 	if(mysqli_query($link,$insertSQL)){
 		echo "Item added successfully";
+		$counter++;
 	} else{
 		echo "Error: invalid query".mysqli_error($link);
 	}
