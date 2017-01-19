25d28d2f931bf74a20fe2925d678c2bd42145a61
Daniel Warrick <ea2758@wayne.edu>
2017-01-19 02:09:19 -0800

    Expanded on the comments for both files.

--- Connection.php
+++ Connection.php
@@ -1,8 +1,10 @@
 <?php
-
+//creates connection to host
 $link = mysqli_connect("localhost","root","");
+//tests connection; reports failure when appropriate
 if($link==false){
 	die("Error: connection error");
 }
+//accesses the database; reports failure when appropriate
 mysqli_select_db($link,"todolist") or die("error: cannot locate database");
 ?>
--- ToDoList.php
+++ ToDoList.php
@@ -1,4 +1,8 @@
 <?php
+//Username: Root  
+//Host: localhost
+
+//path for directory - must be changed if running locally
 require 'C:/wamp64/www/ToDoList/connection.php'
 ?>
 <html>
@@ -30,23 +34,39 @@ require 'C:/wamp64/www/ToDoList/connection.php'
 </body>
 </html>
 <?php
-//Add Item Button
+/***********************************************
+Add Item Button
+-Reads input from a text field adjacent to the 
+button. This input is then inserted into the table
+containing uncompleted tasks. 
+-Executes when the button is clicked
+**********************************************/
 if(isset($_POST["addItem"])&&!empty($_POST["addItem"])){
-	
+	//gets data from input field
 	$item = mysqli_real_escape_string($link, $_POST["newItem"]);
+	//query for inserting data into uncompleted_tasks table
 	$insertSQL = "INSERT INTO uncompleted_tasks(uncompleted_task)VALUES('$item')";
+	//executes query/outputs success
 	if(mysqli_query($link,$insertSQL)){
-		echo "Item added successfully\n\n";	
+		echo "Item added successfully";	
 	} else{
 		echo "Error: invalid query".mysqli_error($link);
 	}
 }
-//View To Do List Button
+/***********************************************
+View To Do List Button
+-Prints the contents of the uncompleted task table 
+in the form of: task_number		task 
+-Executes when the button is clicked
+***********************************************/
 if(isset($_POST["viewList"])&&!empty($_POST["viewList"])){
+	//query to print contents of uncompleted task table
 	$printSQL = "SELECT * FROM uncompleted_tasks";
+	//execution of print query
 	$printData = mysqli_query($link,$printSQL);
-		
+		//loops through table to print each row's contents
 		while($printer = mysqli_fetch_array($printData)){
+			//allows text formatting (\t, \n, etc.)
 			echo '<pre>';
 			echo $printer['task_number'];
 			echo "\t";
@@ -55,11 +75,18 @@ if(isset($_POST["viewList"])&&!empty($_POST["viewList"])){
 			echo '</pre>';
 		}
 }
-//View Completed Tasks Button
+/***********************************************
+View Completed Tasks Button
+-Prints the contents of the completed task table
+in the form of: task_number		task
+-Executes when the button is clicked
+***********************************************/
 if(isset($_POST["viewCompleted"])&&!empty($_POST["viewCompleted"])){
+	//query to print contents of the table
 	$printSQL = "SELECT * FROM completed_tasks";
+	//execution of query
 	$printData = mysqli_query($link,$printSQL);
-		
+		//loop to print each row
 		while($printer = mysqli_fetch_array($printData)){
 			echo '<pre>';
 			echo $printer['task_number'];
@@ -69,7 +96,15 @@ if(isset($_POST["viewCompleted"])&&!empty($_POST["viewCompleted"])){
 			echo '</pre>';
 		}
 }
-//Delete Task Button
+/***********************************************
+Delete Task Button
+-Reads input data from the text field adjacent to 
+the button.
+-This input data represents the task number of the
+element to be removed.
+-The element is first copied into the completed_tasks
+table, then it is deleted from the original table 
+***********************************************/
 if(isset($_POST["completedItem"])&&!empty($_POST["completedItem"])){
 	//get value from input field (task_number of element to be removed)
 	$itemDone = mysqli_real_escape_string($link, $_POST["completedItem"]);
@@ -84,8 +119,9 @@ if(isset($_POST["completedItem"])&&!empty($_POST["completedItem"])){
 	if(mysqli_query($link,$insertSQL)){
 		
 	} 
-	
+	//query to delete the value from the original table
 	$removeSQL = "DELETE FROM uncompleted_tasks WHERE task_number='$itemDone'";
+	//execution of the query/output success
 	if(mysqli_query($link,$removeSQL)){
 		echo "Item removed successfully and added to completed list.";	
 	} else{
@@ -94,13 +130,22 @@ if(isset($_POST["completedItem"])&&!empty($_POST["completedItem"])){
 	
 		
 }
+/***********************************************
+Reset All Tables
+-Uses several queries to remove all existing data
+from both tables.
+-It then resets the AUTO_INCREMENT feature for 
+both tables
+***********************************************/
 if(isset($_POST["clearAll"])&&!empty($_POST["clearAll"])){
 	echo "All stored data has been cleared.";
+	//queries to delete all rows in uncompleted_tasks and completed_tasks
 	$wipeTableSQL = "DELETE FROM `uncompleted_tasks`";
 	$wipeTable2SQL = "DELETE FROM `completed_tasks`";
+	//queries to reset auto_increment
 	$resetIncrementSQL = "ALTER TABLE `uncompleted_tasks` AUTO_INCREMENT=1";
 	$resetIncrement2SQL = "ALTER TABLE `completed_tasks` AUTO_INCREMENT=1";
-	
+	//query execution
 	mysqli_query($link,$wipeTableSQL);
 	mysqli_query($link,$wipeTable2SQL);
 	mysqli_query($link,$resetIncrementSQL);
